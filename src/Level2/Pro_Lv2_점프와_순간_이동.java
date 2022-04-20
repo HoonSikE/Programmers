a3708a06fb3a76b6c74dcb6e86fa92c13750c558
HoonSikE <hoongrammer@gmail.com>
2022-04-20 21:57:52 +0900

    Update Pro_Lv2_점프와_순간_이동.java

--- src/Level2/Pro_Lv2_점프와_순간_이동.java
+++ src/Level2/Pro_Lv2_점프와_순간_이동.java
@@ -6,9 +6,11 @@ package Level2;
  */
 public class Pro_Lv2_점프와_순간_이동 {
 	public static void main(String[] args) {
-		
+		System.out.println(solution(5));
+		System.out.println(solution(6));
+		System.out.println(solution(5000));
 	}
-    public int solution(int n) {
+    public static int solution(int n) {
         int ans = 0;
         while(n >= 1){
             if(n%2 == 1){
